dd6cae72428085d610cbca6ac540a30e1bc08b7e
ashcodes101010 <ashleighcoltman@college.harvard.edu>
2021-01-09 22:45:01 -0500

    Updates

---
+++ precache-manifest.5c87a91dd7234caeeba2b402b01d3bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cb1df707a7dc5a33edfeea490878b0",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "d2ecc4ae21f40ddd3c9c",
+    "url": "/personal-assistant/static/js/2.dd2c35aa.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/personal-assistant/static/js/2.dd2c35aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a429c044eeafd61709",
+    "url": "/personal-assistant/static/js/main.e7baf57e.chunk.js"
+  },
+  {
+    "revision": "b8b8e24a3d2cb0a90037",
+    "url": "/personal-assistant/static/js/runtime-main.c67efb09.js"
+  }
+]);
